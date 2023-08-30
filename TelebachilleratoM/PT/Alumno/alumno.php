0dbe08c7a90fb38668d14a2a140230a9bf4abff7
Esteban-LC <102296285+Esteban-LC@users.noreply.github.com>
2023-08-30 16:25:20 -0600

    carga de archivos
    avance de archivos a utilizar

---
+++ TelebachilleratoM/PT/Alumno/alumno.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/estilos/estilo.css">
+  <title>Plataforma</title>
+</head>
+
+<body>
+  <header>
+    <div>
+      <nav>
+        <figure class="logo"></figure>
+      </nav>
+    </div>
+  </header>
+  <footer>
+    <div>
+
+    </div>
+  </footer>
+</body>
+
+</html>
